627e502dc29491c5b6a49ce2bfc789e1f5c7d25d
Dwaylan <Dwaylan.Applewhite@gmail.com>
2024-12-18 15:05:41 -0500

    Chore: updated firebase config for v 11.1

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,11 @@
 console.log("now connected");
+
+db.collection("recipes")
+  .get()
+  .then((snapshot) => {
+    // When we have the data
+    console.log(snapshot);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
