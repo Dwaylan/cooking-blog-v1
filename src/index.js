96f5985b570a6fcd46d157cac8d2721a534618cd
Dwaylan <Dwaylan.Applewhite@gmail.com>
2024-12-19 10:40:40 -0500

    Chore: reconfigured firebase connection for v11.1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEZw_aa293GOnZMQy2HKSEoKmGCKVcVY8",
+  authDomain: "first-project-a81c0.firebaseapp.com",
+  projectId: "first-project-a81c0",
+  storageBucket: "first-project-a81c0.firebasestorage.app",
+  messagingSenderId: "816395632076",
+  appId: "1:816395632076:web:128d0da2548095b57f7169",
+  measurementId: "G-WTJY5S17CC",
+};
+
+initializeApp(firebaseConfig);
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// Bundling imports from src/index.js into one file
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  watch: true,
+};
